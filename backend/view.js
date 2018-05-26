e62172c43480e1466e8f54b83f09cbce670809a0
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-05-26 13:26:04 -0400

    Code cleanup on view.js

--- backend/view.js
+++ backend/view.js
@@ -1,20 +1,21 @@
 const l = process.argv[2];
 
+const pull = require('pull-stream');
 const ssbClient = require('ssb-client');
 const ssbKeys = require('ssb-keys');
 const ssbFeed = require('ssb-feed');
-const pull = require('pull-stream');
 
-const keyz = ssbKeys.load('/var/www/backend/keys/' + l , function(err, k) {
-        ssbClient(keyz,
-        function (err, sbot) {
-		    pull(
-                sbot.createHistoryStream({ id: k.id, live: false } ),
-                pull.filter(msg => typeof msg.value.content === 'object' && !msg.value.content.root),
-                pull.collect((err, msgs) => {
-                    console.log(JSON.stringify(msgs));
-                    sbot.close();
-                })
-            )
-        })
-})
+const keyz = ssbKeys.load('/var/www/backend/keys/' + l, function(err, k) {
+    ssbClient(keyz, function(err, sbot) {
+        pull(
+          sbot.createHistoryStream({id: k.id}),
+          pull.filter(
+              msg => typeof msg.value.content === 'object' && !msg.value.content.root
+          ),
+          pull.collect((err, msgs) => {
+              console.log(JSON.stringify(msgs));
+              sbot.close();
+          })
+        );
+    });
+});
