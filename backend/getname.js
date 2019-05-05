8d5ae80d31fa53d616749a76ec107e391adbf175
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-05-05 11:14:59 -0400

    Removed uneceesary links

--- backend/getname.js
+++ backend/getname.js
@@ -5,11 +5,7 @@ var ssbClient = require('ssb-client')
 var ssbKeys = require('ssb-keys')
 var ssbFeed = require('ssb-feed')
 
-var sb = require('scuttlebot');
-
-
 var keyz = ssbKeys.load('/var/www/backend/keys/' + l , function(err, k) {
-        ssb=sb.createClient({ "keys": keyz });
         ssbClient(keyz,
           function (err, sbot) {
 
